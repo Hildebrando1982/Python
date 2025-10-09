ffe620b6cbb6112a84fb3e312b011097b6198761
Hildebrando Filho <hmarques1982@gmail.com>
2025-10-09 10:26:40 -0300

    Create Senha.py
    Crie um programa que se insere a senha.
    Se a senha for verdadeira exiba Senha correta
    Caso contrário exiba Senha incorreta.
    
    A senha correta é 12345senha

---
+++ Senha.py
@@ -0,0 +1,14 @@
+'''
+Crie um programa que se insere a senha.
+Se a senha for verdadeira exiba Senha correta
+Caso contrário exiba Senha incorreta.
+
+A senha correta é 12345senha
+'''
+
+senha = input('Digite a senha: ')
+
+if senha == '12345seis':
+    print('Senha correta')
+else:
+    print('Senha incorreta')
