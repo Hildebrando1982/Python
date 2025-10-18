a7cdbbb169d6f1eded491a9e2d3efcf6214ff401
Hildebrando Filho <hmarques1982@gmail.com>
2025-10-18 10:06:32 -0300

    origem_do_produto_e_procedencia.py
    Faça um programa que leia o código de origem de um produto e mostre a sua procedência. A procedência obedece a tabela a seguir:
    
    1 ou 2  Sul
    3 ou 4 Sudeste
    5 ou 6 Norte
    7 ou 8 Nordeste
    9 ou 10 Centro-Oeste

---
+++ origem_do_produto_e_procedencia.py
@@ -0,0 +1,20 @@
+'''
+faça um programa que leia o código de origem de um produto e mostre a sua procedência. A procedência obedece a tabela a seguir:
+
+1 ou 2  Sul
+3 ou 4 Sudeste
+5 ou 6 Norte
+7 ou 8 Nordeste
+9 ou 10 Centro-Oeste
+'''
+
+if codigo == 1 or codigo == 2:
+    print ("Sul")
+if codigo == 3 or codigo == 4:
+    print ("Sudeste")
+if codigo == 5 or codigo == 6:
+    print ("Norte")
+if codigo == 7 or codigo == 8:
+    print ("Nordeste")
+if codigo == 9 or codigo == 10:
+    print ("Contro-Oeste")
