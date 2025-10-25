18d72585722ca6596f0e24ad24273789bb7e518d
Hildebrando Filho <hmarques1982@gmail.com>
2025-10-25 18:41:42 -0300

    Update Celsius em Fahrenheit.py

--- Celsius em Fahrenheit.py
+++ Celsius em Fahrenheit.py
@@ -2,10 +2,18 @@
 Converter ºC para ºF
 '''
 
-#insere os dados da temperatura em Celsius (ºC)
-celsius = float(input('Digite a temperatura em Celsius: '))
+def celsius_para_fahrenheit():
+  try:
+    #insere os dados da temperatura em Celsius (ºC)
+    celsius = float(input('Digite a temperatura em Celsius: '))
 
-#a fórmula para converter Celsius para Fahrenheit
-fahrenheit = (celsius * (9/5)) + 32
+    #a fórmula para converter Celsius para Fahrenheit
+    fahrenheit = (celsius * (9/5)) + 32
+      
+    print(f'{celsius:.1f}ºC é {fahrenheit:.1f}ºF')
 
-print(f'{celsius:.1f}ºC é {fahrenheit:.1f}ºF')
+  except ValueError:
+    #captura o erro em caso de digitação de letras ou simbolos no lugar de número
+    print('Dados incorretos. Por favor, digitar um valor numerico.')
+
+celsius_para_fahrenheit()
