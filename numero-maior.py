5702c533968524321cff040d52fd5d358cf7911e
Hildebrando Filho <hmarques1982@gmail.com>
2025-10-30 00:19:58 -0300

    Create numero-maior.py

---
+++ numero-maior.py
@@ -0,0 +1,16 @@
+'''
+Digite 3 números e exiba o maior dele:
+'''
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+n3 = int(input('Digite o terceiro número: '))
+
+if n1 > n2 and n1 > n3:
+    print(n1)
+elif n2 > n1 and n2 > n3:
+    print(n2)
+elif n3 > n1 and n3 > n2:
+    print(n3)
+else:
+    print('Os três números são iguais') 
