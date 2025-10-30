a5fb388a78d686f99e2c55cccad09bde0311700d
Hildebrando Filho <hmarques1982@gmail.com>
2025-10-29 23:56:41 -0300

    Add program to calculate area of a circle

---
+++ area-circulo.py
@@ -0,0 +1,8 @@
+'''
+Faça um programa para calcular a área de um circulo.
+'''
+
+raio = float(input('Digite o valor do raio: '))
+pi = 3.14
+area = pi * (raio * raio)
+print(f'A área de um círculo é {area:.2f}')
